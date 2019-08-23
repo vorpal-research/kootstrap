6c4a2869e18decbf07078394ff5b0cb5ba1d9dd6
stepanov <stepanov@localhost>
2019-08-23 16:19:03 +0300

    Now we dont need to compile compilerFATJAR

--- src/main/kotlin/org/jetbrains/kootstrap/FooBarCompiler.kt
+++ src/main/kotlin/org/jetbrains/kootstrap/FooBarCompiler.kt
@@ -11,20 +11,19 @@ import com.intellij.pom.tree.TreeAspect
 import com.intellij.psi.codeStyle.CodeStyleManager
 import com.intellij.psi.impl.source.codeStyle.IndentHelper
 import com.intellij.psi.impl.source.tree.TreeCopyHandler
-import com.intellij.psi.search.GlobalSearchScope
 import org.apache.commons.cli.CommandLine
 import org.jetbrains.kootstrap.idea.MockCodeStyleManager
 import org.jetbrains.kootstrap.idea.MockIndentHelper
 import org.jetbrains.kootstrap.util.*
 import org.jetbrains.kotlin.cli.common.CLIConfigurationKeys
+import org.jetbrains.kotlin.cli.common.config.addKotlinSourceRoots
 import org.jetbrains.kotlin.cli.common.messages.MessageCollector
 import org.jetbrains.kotlin.cli.jvm.compiler.*
 import org.jetbrains.kotlin.cli.jvm.config.addJvmClasspathRoots
 import org.jetbrains.kotlin.config.CommonConfigurationKeys
 import org.jetbrains.kotlin.config.CompilerConfiguration
-import org.jetbrains.kotlin.config.addKotlinSourceRoots
 import org.jetbrains.kotlin.config.languageVersionSettings
-import org.jetbrains.kotlin.load.java.JvmAbi
+import org.jetbrains.kotlin.metadata.jvm.deserialization.JvmProtoBufUtil
 import org.jetbrains.kotlin.psi.KtFile
 import org.jetbrains.kotlin.resolve.BindingContext
 import org.jetbrains.kotlin.utils.PathUtil
@@ -52,14 +51,13 @@ object FooBarCompiler {
         return TopDownAnalyzerFacadeForJVM.analyzeFilesWithJavaIntegration(
                 env.project,
                 files,
-                CliLightClassGenerationSupport.CliBindingTrace(),
+                CliBindingTrace(),
                 cfg,
                 { scope -> JvmPackagePartProvider(env.configuration.languageVersionSettings, scope) }
         ).bindingContext
     }
 
-    fun setupMyCfg(
-            cmd: CommandLine): CompilerConfiguration {
+    fun setupMyCfg(cmd: CommandLine): CompilerConfiguration {
 
         val cfg = CompilerConfiguration()
 
@@ -80,7 +78,7 @@ object FooBarCompiler {
         cfg.addKotlinSourceRoots(cmd.targetRoots)
 
         cfg.put(CLIConfigurationKeys.MESSAGE_COLLECTOR_KEY, MessageCollector.NONE)
-        cfg.put(CommonConfigurationKeys.MODULE_NAME, JvmAbi.DEFAULT_MODULE_NAME)
+        cfg.put(CommonConfigurationKeys.MODULE_NAME, JvmProtoBufUtil.DEFAULT_MODULE_NAME)
 
         return cfg
     }
